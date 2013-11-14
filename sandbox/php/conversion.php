c922ef5e1bb7dc6835c6b6600dcdb5d34379d2cc
Gregory Becchio <gbecchio@yahoo.fr>
2013-11-14 10:16:59 +0100

    update conversion

--- sandbox/php/conversion.php
+++ sandbox/php/conversion.php
@@ -1,5 +1,8 @@
 <?php
 error_reporting(E_ALL);
+$titre = "<br><br>%s<br><br>";
+$separation = "<br><br>";
+$pre = "<pre>%s</pre>";
 $a = 10;
 var_dump($a);
 echo gettype($a);
@@ -22,7 +25,7 @@ if($a < array())
 {
   echo "tableau toujours suppérieur";
 }
-
+/*
 class A
 {
   public function __tostring()
@@ -38,5 +41,40 @@ if($a < new A())
 if(array() < new A())
 {
   echo "<br /> la classe est toujours suppérieure au tableau";
-}
+}*/
 echo "icccci";
+
+printf("$titre", "<em>Conversion Tableaux</em>");
+$a = 2;
+$b = (array)$a;
+$txt = <<<TXT
+* entier vers array
+** \$a = 2;
+** \$b = (array)\$a;
+TXT;
+echo $separation;
+printf("$pre", "$txt");
+print_r($b);
+$a = array(1, 2, 3);
+$b = (bool) $a;
+$x = array();
+$y = (bool) $x;
+echo $separation;
+$txt = <<<TXT
+* array vers boolean
+** \$a = array(1, 2, 3);
+** \$b = (bool)\$a; == TRUE
+
+* array vers boolean
+** \$x = array();
+** \$y = (bool)\$x; == FALSE
+
+TXT;
+echo $separation;
+printf("$pre", "$txt");
+echo '$b=>';
+var_dump($b);
+echo "<br>";
+echo '$y=>';
+var_dump($y);
+
