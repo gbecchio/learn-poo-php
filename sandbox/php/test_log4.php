0070ffe6393c7c5dec9add2391462e67f8e3575f
Grégory <gbecchio@yahoo.fr>
2014-01-15 23:43:08 +0100

    use of the inhéritance of the appender

--- sandbox/php/test_log4.php
+++ sandbox/php/test_log4.php
@@ -2,5 +2,6 @@
 include(__DIR__.'/../../apache-log4php/src/main/php/Logger.php');
 // Include and configure log4php
 Logger::configure('config4.xml');
-$main = Logger::getLogger('fol');
+$main = Logger::getLogger('foo');
 $main->info('This will be logged twice.');
+$main->error('This will be logged twice.');
