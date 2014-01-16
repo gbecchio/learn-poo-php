7a954ec2a2551d02d6037077aa383d354521b92f
Gregory Becchio <gbecchio@yahoo.fr>
2014-01-16 13:09:38 +0100

    dev

--- sandbox/php/test_log4.php
+++ sandbox/php/test_log4.php
@@ -2,6 +2,6 @@
 include(__DIR__.'/../../apache-log4php/src/main/php/Logger.php');
 // Include and configure log4php
 Logger::configure('config4.xml');
-$main = Logger::getLogger('foo');
+$main = Logger::getLogger('quatre');
 $main->info('This will be logged twice.');
 $main->error('This will be logged twice.');
